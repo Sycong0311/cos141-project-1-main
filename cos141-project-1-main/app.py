9a436d00959a9ae1a8d11473f470846f0efc5696
Cong Nguyen <congzehel@gmail.com>
2024-12-03 16:45:23 -0500

    asjdniq

--- cos141-project-1-main/app.py
+++ cos141-project-1-main/app.py
@@ -157,7 +157,7 @@ def profile():
         # we need all the account info for the user so we can display it on the profile page
         cursor.execute("SELECT * FROM accounts WHERE id = %s", [session["id"]])
         account = cursor.fetchone()
-# ddd
+
         # show the profile page with account info
         return render_template("profile.html", account=account)
 
